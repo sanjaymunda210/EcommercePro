0fe46ab55b157aeee9d3c09d31f5529e02e5fa35
Sanjay <sanjaymunda0123@gmail.com>
2023-07-13 13:11:06 +0530

    added Catagory Section with add Catagory functionality

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Catagory;
+
+class AdminController extends Controller
+{
+    public function view_catagory()
+    {
+        return view('admin.catagory');
+    }
+    public function add_catagory(Request $request)
+    {
+        // echo '<pre>';
+        // print_r($request->all());
+        // echo '</pre>';
+        $data = new Catagory;
+        $data->catagory_name = $request['catagory'];
+        $data->save();
+
+        return redirect()->back()->with('message', 'Catagory Added Successfully');
+    }
+}
---
+++ resources/views/admin/catagory.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Admin</title>
+    <!-- plugins:css -->
+    @include('admin.css')
+    <style type="text/css">
+        .div_center {
+            text-align: center;
+            padding-top: 30px;
+        }
+
+        .h2_font {
+            font-size: 30px;
+            padding-bottom: 30px;
+        }
+
+        .input_color {
+            color: #000;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_navbar.html -->
+            @include('admin.navbar')
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+
+                    @if (session()->has('message'))
+                        <div class="alert alert-success">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                            {{ session()->get('message') }}
+                        </div>
+                    @endif
+
+                    <div class="div_center">
+                        <h2 class="h2_font">Add Catagory</h2>
+
+                        <form action="{{ url('add_catagory') }}" method="post">
+                            @csrf
+                            <input type="text" class="input_color " name="catagory"
+                                placeholder="Enter Catagory Name">
+                            <input type="submit" class="btn btn-primary" value="Add catagory">
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.scripts')
+    <!-- End custom js for this page -->
+</body>
+
+</html>
