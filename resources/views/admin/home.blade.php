666c1b64d7c30704b1255718f01b11637a83d25d
Sanjay <sanjaymunda0123@gmail.com>
2023-07-13 11:59:15 +0530

    added admin template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,33 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
 
-</x-app-layout>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Admin</title>
+    <!-- plugins:css -->
+    @include('admin.css')
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_navbar.html -->
+            @include('admin.navbar')
+            <!-- partial -->
+            @include('admin.dashboard')
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.scripts')
+    <!-- End custom js for this page -->
+</body>
+
+</html>
